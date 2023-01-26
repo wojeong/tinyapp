e8a21555e29517c88bef2462a71f230114269476
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-26 04:23:10 -0500

    Random String Generator Implemented

--- express_server.js
+++ express_server.js
@@ -2,15 +2,39 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+//Random String Generator that generates a string with the length of 6
+const generateRandomString = function() {
+  let result = '';
+  
+  //a String that holds all number and character
+  const allChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  //Randomly picks a charater or number from the allChar 6 times.
+  for(let i = 0; i < 6; i ++) {
+    result += allChar.charAt(Math.floor(Math.random() * allChar.length));
+  }
+  return result;
+}
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(generateRandomString()); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
