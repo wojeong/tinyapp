021789d287a3458be0b8f482781ae498a2cbf250
Wo Yong Jeong <woyong.jeong@gmail.com>
2023-01-26 04:58:06 -0500

    Delete implemented

--- express_server.js
+++ express_server.js
@@ -46,6 +46,13 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.params.id);
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
